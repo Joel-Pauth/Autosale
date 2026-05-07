a892c18bdf25af7bb694d1c7448df600447def3b
Joel-Pauth <http://joelantoniopauthgonzalez@gmail.com>
2026-05-06 21:56:17 -0600

    Agregar datos en el modulo brands

--- src/modules/brands/controllers/brands.controller.ts
+++ src/modules/brands/controllers/brands.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BrandsService } from '../services/brands.service';
+import { CreateBrandDto } from '../dto/brand.dto';
 
 @Controller('brands')
-export class BrandsController {}
+export class BrandsController {
+  constructor(private readonly brandsService: BrandsService) {}
+
+  @Post()
+  create(@Body() createBrandDto: CreateBrandDto) {
+    return this.brandsService.create(createBrandDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.brandsService.findAll();
+  }
+}
