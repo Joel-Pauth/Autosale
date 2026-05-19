7ab85b6f3eaaf1b5091fd62feb00fdc2d4611c0a
Joel-Pauth <http://joelantoniopauthgonzalez@gmail.com>
2026-05-18 21:02:46 -0600

    fix: construccion de sales y customers y peticion en postman

--- src/modules/customers/customers.module.ts
+++ src/modules/customers/customers.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
+import { Customer } from './entities/customers.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Customer])],
   controllers: [CustomersController],
   providers: [CustomersService],
 })
--- src/modules/sales/sales.module.ts
+++ src/modules/sales/sales.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SalesController } from './controllers/sales.controller';
 import { SalesService } from './services/sales.service';
+import { Sale } from './entities/sales.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Sale])],
   controllers: [SalesController],
   providers: [SalesService],
 })
